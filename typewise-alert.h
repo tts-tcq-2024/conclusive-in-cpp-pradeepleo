6e4da7a8939dd48d16250de8e8c6e0b3209d8d8c
Pradeep_03 <51703548+pradeepleo@users.noreply.github.com>
2024-11-02 10:40:45 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,32 +1,44 @@
 #pragma once
 
+// Enum for cooling types
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
   MED_ACTIVE_COOLING
 } CoolingType;
 
+// Enum for breach types
 typedef enum {
   NORMAL,
   TOO_LOW,
   TOO_HIGH
 } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
-
+// Enum for alert targets
 typedef enum {
   TO_CONTROLLER,
   TO_EMAIL
 } AlertTarget;
 
+// Struct to represent the battery characteristics
 typedef struct {
   CoolingType coolingType;
   char brand[48];
 } BatteryCharacter;
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+// Struct to hold temperature limits
+typedef struct {
+  int lowerLimit;
+  int upperLimit;
+} TemperatureLimits;
 
+// Function declarations
+TemperatureLimits getTemperatureLimits(CoolingType coolingType);
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
+void notifyByEmail(const char* message);
+
+#endif // TYPEWISE_ALERT_H
